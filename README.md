1300d6e9fbb6bbdca5ef3e89d94962cdfccf437c
Kefkius <kefkius@mail.com>
2018-03-04 15:59:52 -0500

    Add API and PyPI data

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Grøstl
 ======
 
 This is a pure python implementation of Grøstl written for educational purposes.
+
+Usage
+-----
+
+```python
+import groestlcoin_hash2
+print(groestlcoin_hash2.groestl_hash(b'\x01'))
+```
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'groestlcoin_hash2',
+    version = '1.0',
+    description = 'Pure python Groestlcoin hash algorithm.',
+    maintainer = 'Kefkius',
+    maintainer_email = 'kefkius@mail.com',
+    url = 'https://github.com/groestlcoin/groestlcoin-hash-pure-python',
+    keywords = ['groestlcoin', 'grs'],
+    packages = ['groestlcoin_hash2']
+)
